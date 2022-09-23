73b999344aff21226acb4dfde8881734f04c3c35
safadtm <safadtm2004@gmail.com>
2022-09-23 12:10:12 +0530

    Home Page AppBar UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/home/main_food_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: const MyHomePage(),
+      home: const MainFoodPage(),
     );
   }
 }
