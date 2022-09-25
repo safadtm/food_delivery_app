c71522c07bdba7729133619cfa0c8b461ec16eb8
safadtm <safadtm2004@gmail.com>
2022-09-25 21:17:21 +0530

    .

---
+++ lib/data/controller/popular_product_controller.dart
@@ -0,0 +1,6 @@
+import 'package:food_delivery_app/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductrepo;
+}
